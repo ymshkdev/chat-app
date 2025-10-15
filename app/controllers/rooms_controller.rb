4201d51381f7628c355075e58111de4c83cebad5
ymshkdev <masahiko.yonemura@outlook.jp>
2025-10-15 18:05:32 +0900

    新規作成画面の表示

---
+++ app/controllers/rooms_controller.rb
@@ -0,0 +1,6 @@
+class RoomsController < ApplicationController
+def new
+  @rooms = Room.new
+end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   root to: "messages#index"
   devise_for :users
   resources :users, only: [:edit, :update]
+  resources :rooms, only: [:new, :create]
   get "up" => "rails/health#show", as: :rails_health_check
 
 end
