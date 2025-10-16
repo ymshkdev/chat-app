73bae99ece6aec5fcfe35e0a7ad7cac6c518ab40
ymshkdev <masahiko.yonemura@outlook.jp>
2025-10-16 14:07:27 +0900

    チャット画面の編集

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
   root to: "rooms#index"
   devise_for :users
   resources :users, only: [:edit, :update]
-  resources :rooms, only: [:new, :create]
+  resources :rooms, only: [:new, :create] do
+   resources :messages, only: [:index,:create]
+  end
   get "up" => "rails/health#show", as: :rails_health_check
 
 end
